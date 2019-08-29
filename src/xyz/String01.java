776d82139cab0ec7c7c34fabae495eb14b3c7dfe
reaz-majumder <reaz.majumder00@gmail.com>
2019-08-28 23:33:58 -0700

    exampleCommit

---
+++ src/xyz/String01.java
@@ -0,0 +1,9 @@
+package xyz;
+
+public class String01 {
+    public static void main(String[] args) {
+        String name= "riaz";
+        int id =10;
+        System.out.println(name+id);
+    }
+}
